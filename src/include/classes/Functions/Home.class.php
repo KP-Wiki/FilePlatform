0c315126ecb808e393842eed3a7df3bd434334b6
Thimo Braker <thibmorozier@gmail.com>
2017-03-13 02:28:49 +0100

    Change to sortable, filterable and paginated table with support for the FileDetail feature
    Signed-off-by: Thimo Braker <thibmorozier@gmail.com>

--- src/include/classes/Functions/Home.class.php
+++ src/include/classes/Functions/Home.class.php
@@ -32,48 +32,49 @@
             $dbHandler -> PrepareAndBind ($query);
             $mapListItems = $dbHandler -> ExecuteAndFetchAll();
 
-            $content = '<div class="panel panel-default">' . PHP_EOL .
-                       '    <div class="panel-heading">Knights Province Map database</div>' . PHP_EOL .
-                       '    <table class="table .filetable">' . PHP_EOL .
-                       '        <thead>' . PHP_EOL .
-                       '            <tr>' . PHP_EOL .
-                       '                <th class="col-xs-2">Name</th>' . PHP_EOL .
-                       '                <th class="col-xs-2">Author</th>' . PHP_EOL .
-                       '                <th class="col-xs-1">Version</th>' . PHP_EOL .
-                       '                <th class="col-xs-3">Description</th>' . PHP_EOL .
-                       '                <th class="col-xs-2">Last Updated</th>' . PHP_EOL .
-                       '                <th class="col-xs-2">Screenshot</th>' . PHP_EOL .
-                       '                <th class="col-xs-1">Downloads</th>' . PHP_EOL .
-                       '                <th class="col-xs-1"></th>' . PHP_EOL .
-                       '                <th class="col-xs-1"></th>' . PHP_EOL .
-                       '            </tr>' . PHP_EOL .
-                       '        </thead>' . PHP_EOL .
-                       '        <tbody>' . PHP_EOL;
+            $content = '                    <div class="panel panel-default">' . PHP_EOL .
+                       '                        <div class="panel-heading">Knights Province Map database</div>' . PHP_EOL .
+                       '                        <table class="table table-striped table-bordered table-hover" ' .
+                       'data-toggle="table" ' .
+                       'data-search="true" ' .
+                       'data-sort-name="name" ' .
+                       'data-sort-order="desc" ' .
+                       'data-show-toggle="true" ' .
+                       'data-show-columns="true" ' .
+                       'data-minimum-count-columns="2" ' .
+                       'data-show-pagination-switch="true" ' .
+                       'data-pagination="true" ' .
+                       'data-page-list="[10, 25, 50, 100, ALL]" ' .
+                       'data-show-footer="false" ' .
+                       'data-side-pagination="client">' . PHP_EOL .
+                       '                            <thead>' . PHP_EOL .
+                       '                                <tr>' . PHP_EOL .
+                       '                                    <th class="col-xs-2" data-field="type" data-sortable="true">Type</th>' . PHP_EOL .
+                       '                                    <th class="col-xs-2" data-field="name" data-sortable="true">Name</th>' . PHP_EOL .
+                       '                                    <th class="col-xs-2" data-field="author" data-sortable="true">Author</th>' . PHP_EOL .
+                       '                                    <th class="col-xs-4" data-field="description" data-sortable="false">Description</th>' . PHP_EOL .
+                       '                                    <th class="col-xs-1" data-field="rating" data-sortable="true">Rating</th>' . PHP_EOL .
+                       '                                    <th class="col-xs-1" data-field="downloads" data-sortable="true">Downloads</th>' . PHP_EOL .
+                       '                                </tr>' . PHP_EOL .
+                       '                            </thead>' . PHP_EOL .
+                       '                            <tbody>' . PHP_EOL;
 
             if ($mapListItems != null) {
                 foreach ($mapListItems as $mapItem) {
-                    $content .= '            <tr>' . PHP_EOL .
-                                '                <td>' . $mapItem['file_name'] . '</td>' . PHP_EOL .
-                                '                <td>' . $mapItem['user_name'] . '</td>' . PHP_EOL .
-                                '                <td>' . $mapItem['rev_file_version'] . '</td>' . PHP_EOL .
-                                '                <td>Short description</td>' . PHP_EOL .
-                                '                <td>' . $mapItem['rev_upload_date'] . '</td>' . PHP_EOL .
-                                '                <td>Thumbnail</td>' . PHP_EOL .
-                                '                <td style="text-align: right;">' . StrVal($mapItem['file_downloads']) . '</td>' . PHP_EOL .
-                                '                <td>' . PHP_EOL .
-                                '                    <button type="submit" title="Download this map">' . PHP_EOL .
-                                '                        <span class="glyphicon glyphicon-download-alt" onclick="window.open(\'/download?file=' . $mapItem['rev_pk'] .
-                                '\', \'popUpWindow\', \'height=400, width=600, left=10, top=10, , scrollbars=yes, menubar=no\'); return false;"></span>' . PHP_EOL .
-                                '                    </button>' . PHP_EOL .
-                                '                </td>' . PHP_EOL .
-                                '                <td><button title="Flag this map"><span class="glyphicon glyphicon-flag"></span></button></td>' . PHP_EOL .
-                                '            </tr>' . PHP_EOL;
+                    $content .= '                                <tr>' . PHP_EOL .
+                                '                                    <td>Type</td>' . PHP_EOL .
+                                '                                    <td><a href="/filedetails?file=' . $mapItem['rev_pk'] . '">' . $mapItem['file_name'] . '</a></td>' . PHP_EOL .
+                                '                                    <td>' . $mapItem['user_name'] . '</td>' . PHP_EOL .
+                                '                                    <td>Short description</td>' . PHP_EOL .
+                                '                                    <td>Rating</td>' . PHP_EOL .
+                                '                                    <td style="text-align: right;">' . StrVal($mapItem['file_downloads']) . '</td>' . PHP_EOL .
+                                '                                </tr>' . PHP_EOL;
                 };
             };
 
-            $content .= '        </tbody>' . PHP_EOL .
-                        '    </table>' . PHP_EOL .
-                        '</div>';
+            $content .= '                            </tbody>' . PHP_EOL .
+                        '                        </table>' . PHP_EOL .
+                        '                    </div>';
 
             return $content;
         }
