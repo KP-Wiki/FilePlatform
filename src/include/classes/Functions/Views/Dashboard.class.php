dd23ac526ae2387d887169953b5329f89eb1df85
Thimo Braker <thibmorozier@gmail.com>
2017-04-01 23:24:12 +0200

    Remove superfluous column
    Signed-off-by: Thimo Braker <thibmorozier@gmail.com>

--- src/include/classes/Functions/Views/Dashboard.class.php
+++ src/include/classes/Functions/Views/Dashboard.class.php
@@ -46,7 +46,6 @@
                         '                                        <th class="col-xs-2" data-field="map_type_name" data-sortable="true">Type</th>' . PHP_EOL .
                         '                                        <th class="col-xs-2" data-field="map_name" data-formatter="detailUrlFormatter"' .
                         ' data-sortable="true">Name</th>' . PHP_EOL .
-                        '                                        <th class="col-xs-2" data-field="user_name" data-sortable="true">Author</th>' . PHP_EOL .
                         '                                        <th class="col-xs-4" data-field="rev_map_description_short" data-sortable="false">Description</th>' . PHP_EOL .
                         '                                        <th class="col-xs-1" data-field="avg_rating" data-sortable="true">Rating</th>' . PHP_EOL .
                         '                                        <th class="col-xs-1" data-field="map_downloads" data-sortable="true">Downloads</th>' . PHP_EOL .
